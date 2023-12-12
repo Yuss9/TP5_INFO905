3a1771bd402b6fbdd423325fcabcec04ca5ed2fd
Yuss9 <yurtseven68210@hotmail.fr>
2023-12-12 14:04:39 +0100

    chore : add comment

--- main.py
+++ main.py
@@ -14,11 +14,14 @@ class QLearningAgent:
     def choose_action(self, state):
         # Politique epsilon-greedy
         if np.random.rand() < self.epsilon:
+            # Exploration : choisissez une action au hasard
             return np.random.choice(self.num_actions)
         else:
+            # Exploitation : choisissez l'action avec la plus grande valeur Q
             return np.argmax(self.q_values[state, :])
 
     def update_q_values(self, state, action, reward, next_state):
+        # Mettre à jour la table Q-valeurs en utilisant la formule de Q-learning
         best_next_action = np.argmax(self.q_values[next_state, :])
         self.q_values[state, action] += self.alpha * \
             (reward + self.gamma *
@@ -46,11 +49,18 @@ def train_q_learning(agent, env, num_episodes):
             print(f"Episode {episode}, Total Reward: {total_reward}")
 
 
+# Créer un environnement
 env = Game(4, 4, wrong_action_p=0.1)
+
+# Créer un agent Q-learning
 num_states = env.n * env.m
 num_actions = len(env.ACTIONS)
 q_learning_agent = QLearningAgent(num_states, num_actions)
+
+# Entraîner l'agent
 train_q_learning(q_learning_agent, env, num_episodes=1000)
+
+# Tester l'agent entraîné
 state = env.reset()
 env.print()
 
